31eaa6fd8d3247b66f9a98796a8e28eb252b986e
S Varun <varunshanker1718@gmail.com>
2025-02-27 14:34:39 +0530

    setup configurations and refactor omnivault package name

--- omnivault-backend/src/main/java/com/personal/omnivault/OmnivaultBackendApplication.java
+++ omnivault-backend/src/main/java/com/personal/omnivault/OmnivaultBackendApplication.java
@@ -1,4 +1,4 @@
-package com.personal.omnivault_backend;
+package com.personal.omnivault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- omnivault-backend/src/test/java/com/personal/omnivault/OmnivaultBackendApplicationTests.java
+++ omnivault-backend/src/test/java/com/personal/omnivault/OmnivaultBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.personal.omnivault_backend;
+package com.personal.omnivault;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
