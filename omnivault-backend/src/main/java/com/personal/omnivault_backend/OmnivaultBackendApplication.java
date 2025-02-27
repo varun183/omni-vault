81c6cd80defdbffa4dd8ce793b140d0b69dba491
S Varun <varunshanker1718@gmail.com>
2025-02-27 12:22:57 +0530

    Setup Spring Boot project with dependencies

---
+++ omnivault-backend/src/main/java/com/personal/omnivault_backend/OmnivaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.personal.omnivault_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnivaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnivaultBackendApplication.class, args);
+	}
+
+}
---
+++ omnivault-backend/src/test/java/com/personal/omnivault_backend/OmnivaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.omnivault_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnivaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
