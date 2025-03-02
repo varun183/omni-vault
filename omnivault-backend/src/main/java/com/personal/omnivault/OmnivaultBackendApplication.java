1e42e89a5215983d2c3514f09bc15778119b1c7a
S Varun <varunshanker1718@gmail.com>
2025-03-02 16:57:28 +0530

    Configure StorageProperties and scheduling

--- omnivault-backend/src/main/java/com/personal/omnivault/OmnivaultBackendApplication.java
+++ omnivault-backend/src/main/java/com/personal/omnivault/OmnivaultBackendApplication.java
@@ -1,13 +1,30 @@
-package com.personal.omnivault;
+	package com.personal.omnivault;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import com.personal.omnivault.config.StorageProperties;
+	import com.personal.omnivault.service.StorageService;
+	import org.springframework.boot.CommandLineRunner;
+	import org.springframework.boot.SpringApplication;
+	import org.springframework.boot.autoconfigure.SpringBootApplication;
+	import org.springframework.boot.context.properties.EnableConfigurationProperties;
+	import org.springframework.context.annotation.Bean;
+	import org.springframework.scheduling.annotation.EnableAsync;
+	import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-public class OmnivaultBackendApplication {
+	@SpringBootApplication
+	@EnableConfigurationProperties(StorageProperties.class)
+	@EnableAsync
+	@EnableScheduling
+	public class OmnivaultBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OmnivaultBackendApplication.class, args);
+		public static void main(String[] args) {
+			SpringApplication.run(OmnivaultBackendApplication.class, args);
+		}
+
+		@Bean
+		CommandLineRunner init(StorageService storageService) {
+			return (args) -> {
+				// Initialize storage location
+				storageService.init();
+			};
+		}
 	}
-
-}
