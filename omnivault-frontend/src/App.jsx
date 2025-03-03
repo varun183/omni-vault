529d4d7e1fae9ef624d98b1943b1dfc0b1042ca0
S Varun <varunshanker1718@gmail.com>
2025-03-03 16:38:17 +0530

    Set up React Router and authentication flow in App.js
    Integrated React Router with BrowserRouter and defined initial routes.
    
    Implemented authentication flow by dispatching getCurrentUser on app load
    
    if an access token exists in local storage.
    
    This ensures a structured navigation system and persistent authentication.

--- omnivault-frontend/src/App.jsx
+++ omnivault-frontend/src/App.jsx
@@ -1,12 +1,28 @@
-import "./App.css";
-import ReduxTester from "./components/testing/ReduxTester";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import "react-toastify/dist/ReactToastify.css";
+import { getCurrentUser } from "./store/slices/authSlice";
+import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localStorage.getItem("access_token")) {
+      dispatch(getCurrentUser());
+    }
+  }, [dispatch]);
+
   return (
-    <>
-      <p>Omnivault</p>
-      {/* {import.meta.env.DEV && <ReduxTester />} */}
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        {/* {import.meta.env.DEV && <ReduxTester />} */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
