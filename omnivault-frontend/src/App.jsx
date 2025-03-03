9c13d18299c697ed817f7cd3f5fa77ceca1586e1
S Varun <varunshanker1718@gmail.com>
2025-03-03 22:07:53 +0530

    Add HomePage routing with authentication check
    Integrated HomePage into routing and ensured it is accessible only for authenticated users. Redirects unauthenticated users to login.

--- omnivault-frontend/src/App.jsx
+++ omnivault-frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
 import { getCurrentUser } from "./store/slices/authSlice";
 import LoginPage from "./pages/LoginPage";
 import Navbar from "./components/layout/Navbar";
 import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
   const dispatch = useDispatch();
+
+  const { isAuthenticated } = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (localStorage.getItem("access_token")) {
@@ -22,6 +25,10 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/"
+          element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />}
+        />
         {/* {import.meta.env.DEV && <ReduxTester />} */}
       </Routes>
     </BrowserRouter>
