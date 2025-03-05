a8d651a2d3be7bce1db81586c00d3380a30a7e52
S Varun <varunshanker1718@gmail.com>
2025-03-06 02:15:40 +0530

    Expand app routes and fix content type filtering
    - Add routes for content, folders, tags, search, favorites and recent pages
    
    - Fix content type filtering by changing route from '/content/:contentType' to '/content-type/:contentType'
    
    - Add toast notifications container for app-wide alerts

--- omnivault-frontend/src/App.jsx
+++ omnivault-frontend/src/App.jsx
@@ -1,16 +1,27 @@
+// src/App.jsx
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { getCurrentUser } from "./store/slices/authSlice";
+
+// Pages
 import LoginPage from "./pages/LoginPage";
-import Navbar from "./components/layout/Navbar";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage";
+import ContentDetailPage from "./pages/ContentDetailPage";
+import FolderPage from "./pages/FolderPage";
+import ContentTypePage from "./pages/ContentTypePage";
+import TagPage from "./pages/TagPage";
+import SearchPage from "./pages/SearchPage";
+import FavoritesPage from "./pages/FavoritesPage";
+import RecentPage from "./pages/RecentPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import TagsPage from "./pages/TagsPage";
 
-function App() {
+const App = () => {
   const dispatch = useDispatch();
-
   const { isAuthenticated } = useSelector((state) => state.auth);
 
   useEffect(() => {
@@ -21,18 +32,43 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+
         <Route
           path="/"
           element={isAuthenticated ? <HomePage /> : <Navigate to="/login" />}
         />
+        <Route path="/content/:contentId" element={<ContentDetailPage />} />
+        <Route path="/folder/:folderId" element={<FolderPage />} />
+        <Route
+          path="/content-type/:contentType"
+          element={<ContentTypePage />}
+        />
+        <Route path="/tags" element={<TagsPage />} />
+        <Route path="/tag/:tagId" element={<TagPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/recent" element={<RecentPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
         {/* {import.meta.env.DEV && <ReduxTester />} */}
       </Routes>
+
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
